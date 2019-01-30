2a78002b2e0321ce05ea292f450810b892ea28cc
Joan Gonzalvez <44616000+jopago@users.noreply.github.com>
2019-01-30 17:33:41 +0100

    Update prox_norms.h

--- prox_norms.h
+++ prox_norms.h
@@ -1,8 +1,8 @@
 #include "prox_norms.h"
 
-extern double asm_proxl2_square(double*,int,double); 
+extern double asm_proxl2_square(double*,int,double); // in-place
 
-double* prox_l2square(double *v, int32_t n, double lambda)
+double* _asm_prox_l2_square(const double *v, int32_t n, double lambda)
 {
 	double* result = malloc(n*sizeof(double));
 	memcpy(res,v,n*sizeof(double));
@@ -12,3 +12,14 @@ double* prox_l2square(double *v, int32_t n, double lambda)
 	return result;
 }
 
+double* c_prox_l2_square(const *double *v, int32_t n, double lambda)
+{
+	double *result = malloc(n*sizeof(double));
+	int i;
+	
+	for(i=0;i<n;i++)
+		result[i] = t[i]/(1+lambda);
+	
+	return result;
+}
+	
