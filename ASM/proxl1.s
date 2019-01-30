3dfa265c94104430445724067343dc3b0937fcd3
Joan Gonzalvez <44616000+jopago@users.noreply.github.com>
2019-01-30 21:16:27 +0100

    Update proxl1.s

--- ASM/proxl1.s
+++ ASM/proxl1.s
@@ -1,32 +1,25 @@
-; not done yet 
-; 
-
 global asm_proxl1
 
 section .text:
 
 asm_proxl1:
+	; proximal operator of l1 norm ("soft-thresholding" operator)
+	; prox_{k ||.||_1} (x) = sgn(x)*max(|x| - k, 0)
+	; it is separable so we can leverage SSE 
+
 	xor rax,rax
 	xorpd xmm2,xmm2
-
-	push rcx
-
-	mov rcx,1
-	shl rcx,63
-
-	cvtsi2sd xmm2,rcx
-
-	;psrlq xmm2,1
-	movlhps xmm2,xmm2  
+	xorpd xmm3,xmm3 ; xmm3 will store constant [0 | 0]
 
 
 	_loop:
-		movapd xmm1,[rdi]
+		subpd xmm2,[rdi]
+		maxpd xmm2,[rdi] 	; xmm2 = [abs(x_i+1) | abs(x_i)]
+		subpd xmm2,xmm0 	; minus lambda 
+		maxpd xmm2,xmm3 	; xmm2 = max(|x| - k, 0)
 
-		andnpd xmm1,xmm2 ; absolute value 
-
-		movapd [rdi],xmm1
-
+		; multiply by sign 
+		
 		add rdi,16
 		add rax, 2
 
