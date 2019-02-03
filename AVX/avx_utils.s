7e1db9b8550b6ad2606c20a436894198246fe9d8
Joan Gonzalvez <44616000+jopago@users.noreply.github.com>
2019-02-03 21:05:31 +0100

    Update avx_utils.s

--- AVX/avx_utils.s
+++ AVX/avx_utils.s
@@ -15,8 +15,8 @@ avx_sum:
 
 	; horizontal sum of 256-bit AVX register is not obvious 
 
-	vhaddpd ymm2,ymm2 	; top and bottom of ymm2 contains two double each 
-						; which correspond to respective top and bottom horizontal sum
+	vhaddpd ymm2,ymm2 	; top and bottom of ymm2 contain two double each 
+				; which correspond to respective top and bottom horizontal sum
 	vextractf128 xmm0,ymm2,0 ; extract bottom double-quadword 
 	vextractf128 xmm1,ymm2,1 ; extract top 
 	addpd xmm0,xmm1 
